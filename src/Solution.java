6b1b1b2c5917f6ce0147573680d334230ff9d322
Moses <netshitanganimoses@gmail.com>
2024-05-10 22:19:20 +0200

    solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 class Node {
     Node left;
@@ -16,7 +17,23 @@ class Node {
 
 class Solution {
     public static void preOrder(Node root) {
+        // root -> left subtree -> right subtree
+        // use a stack ds to dictate order of traversal
+        // push right child first and then left child onto stack
+        // keep looping until stack is empty
 
+        StringBuilder res = new StringBuilder();
+        Stack<Node> nodes = new Stack<>();
+        nodes.push(root);
+
+        while (!nodes.isEmpty()) {
+            Node currentNode = nodes.pop();
+            res.append(currentNode.data).append(" ");
+
+            if (currentNode.right != null) nodes.push(currentNode.right);
+            if (currentNode.left != null) nodes.push(currentNode.left);
+        }
+        System.out.println(res.toString().trim());
     }
 
     public static Node insert(Node root, int data) {
